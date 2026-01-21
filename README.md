0f16a1526a7918ea99c7f5426d0f13d411249804
maharaja <126151224+maharaja04233@users.noreply.github.com>
2026-01-22 01:21:28 +0530

    files added

---
+++ README.md
@@ -0,0 +1,2 @@
+# yk- company
+Learn to build a powerful industrial website with HTML, CSS, and JavaScript!
---
+++ navscript/jquery.rotate.js
@@ -0,0 +1,75 @@
+<<<<<<< HEAD
+;(function($){
+  $.fn.rotate = function(options){
+
+    //默认值
+  var defaults = {
+    speed:10,
+    width:"1110px",
+    slide1:".slide1"
+  }
+  var options = $.extend(defaults, options);
+
+  this.each(function(){
+    var thisR = $(this),
+        thisC = thisR.children();
+    thisR.append('<a href="javascript:;" class="rotate-prev">Prev</a>');
+    thisC.append('<ul class="slide2">'+ $(options.slide1).html() +'</ul>');
+    thisR.css({'overflow':'hidden','width':options.width});
+    thisC.css('width','12000px').children().css('float','left');
+    function Marquee(){
+      if(thisR.scrollLeft() >= $(options.slide1).width()){
+        thisR.scrollLeft(0);
+      }else{
+        thisR.scrollLeft(thisR.scrollLeft()+1);
+      }
+    }
+      var sliding=setInterval(Marquee,options.speed);
+      thisR.hover(function() {
+        //鼠标移动DIV上停止
+        clearInterval(sliding);
+      },function(){
+        //离开继续调用
+        sliding=setInterval(Marquee,options.speed);
+      });
+    });
+
+}})(jQuery);
+=======
+;(function($){
+  $.fn.rotate = function(options){
+
+    //默认值
+  var defaults = {
+    speed:10,
+    width:"1110px",
+    slide1:".slide1"
+  }
+  var options = $.extend(defaults, options);
+
+  this.each(function(){
+    var thisR = $(this),
+        thisC = thisR.children();
+    thisR.append('<a href="javascript:;" class="rotate-prev">Prev</a>');
+    thisC.append('<ul class="slide2">'+ $(options.slide1).html() +'</ul>');
+    thisR.css({'overflow':'hidden','width':options.width});
+    thisC.css('width','12000px').children().css('float','left');
+    function Marquee(){
+      if(thisR.scrollLeft() >= $(options.slide1).width()){
+        thisR.scrollLeft(0);
+      }else{
+        thisR.scrollLeft(thisR.scrollLeft()+1);
+      }
+    }
+      var sliding=setInterval(Marquee,options.speed);
+      thisR.hover(function() {
+        //鼠标移动DIV上停止
+        clearInterval(sliding);
+      },function(){
+        //离开继续调用
+        sliding=setInterval(Marquee,options.speed);
+      });
+    });
+
+}})(jQuery);
+>>>>>>> 1753343 (files added)
